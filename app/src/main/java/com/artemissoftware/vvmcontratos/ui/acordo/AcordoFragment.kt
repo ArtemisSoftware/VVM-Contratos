77bc9a24336b9c6a9cc5cb3a659e3c6ec15d4165
gustavo <gustavo.isaac@gmail.com>
2021-04-03 16:27:41 +0100

    Dependencias: material-spinner

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/acordo/AcordoFragment.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.vvmcontratos.ui.acordo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.artemissoftware.vvmcontratos.R
+
+
+class AcordoFragment : Fragment(R.layout.fragment_acordo) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

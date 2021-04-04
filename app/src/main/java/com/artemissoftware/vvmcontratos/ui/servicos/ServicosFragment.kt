93389d81b90fc3e774d03b5d3524e1ca03c9f788
gustavo <gustavo.isaac@gmail.com>
2021-04-04 15:48:13 +0100

    layout fragment_servicos.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/servicos/ServicosFragment.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.vvmcontratos.ui.servicos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+
+class ServicosFragment : Fragment(R.layout.fragment_servicos) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

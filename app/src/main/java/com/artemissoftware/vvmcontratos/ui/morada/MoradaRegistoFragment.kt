786f822ca19a29b8bcc19fa06e43c214bdbeadfd
gustavo <gustavo.isaac@gmail.com>
2021-04-04 15:39:37 +0100

    layout fragment_morada_registo.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/morada/MoradaRegistoFragment.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.vvmcontratos.ui.morada
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+
+
+class RegistoMoradaFragment : Fragment(R.layout.fragment_morada_registo) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

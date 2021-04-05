719a49b0e0922a1e26d8cd889a40de827dce7071
gustavo <gustavo.isaac@gmail.com>
2021-04-05 16:29:26 +0100

    layout fragment_observacao.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/observacao/ObservacaoFragment.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.vvmcontratos.ui.observacao
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+
+class ObservacaoFragment : Fragment(R.layout.fragment_observacao) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

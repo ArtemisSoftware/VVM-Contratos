9c85060e785a7ed0376cca2ce3b575ea437c204f
gustavo <gustavo.isaac@gmail.com>
2021-04-05 17:01:30 +0100

    layout fragment_rever_concluir.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/rever/ReverConcluirFragment.kt
@@ -0,0 +1,19 @@
+package com.artemissoftware.vvmcontratos.ui.rever
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+class ReverConcluirFragment : Fragment(R.layout.fragment_rever_concluir) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

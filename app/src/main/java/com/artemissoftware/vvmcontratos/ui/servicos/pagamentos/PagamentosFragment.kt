9f9c0e43ebdaec0c0ecffddcd5d6b6c08e23222d
gustavo <gustavo.isaac@gmail.com>
2021-04-06 23:23:57 +0100

    layout fragment_pagamentos.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/servicos/pagamentos/PagamentosFragment.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.vvmcontratos.ui.servicos.pagamentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+
+
+class PagamentosFragment : Fragment(R.layout.fragment_pagamentos) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

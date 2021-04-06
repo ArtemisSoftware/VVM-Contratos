9443f0bae6c1a6428125d577e853d520b3d8f665
gustavo <gustavo.isaac@gmail.com>
2021-04-06 18:07:52 +0100

    layout fragment_extintores.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/servicos/extintores/ExtintoresFragment.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.vvmcontratos.ui.servicos.extintores
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+
+
+
+class ExtintoresFragment : Fragment(R.layout.fragment_extintores) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}

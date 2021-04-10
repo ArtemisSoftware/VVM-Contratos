bedeedb57aab6633f538948e7c3138cfbd967b56
gustavo <gustavo.isaac@gmail.com>
2021-04-10 19:53:05 +0100

    BottomNavigationView parte 1

--- app/src/main/java/com/artemissoftware/vvmcontratos/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/vvmcontratos/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +19,8 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
+
+
+        bottom_nav.setupWithNavController(navController)
     }
 }

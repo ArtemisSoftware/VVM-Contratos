001fa89b315480b580301bb95ec042d58eb298e8
gustavo <gustavo.isaac@gmail.com>
2021-04-12 11:27:36 +0100

    Layout fragment_tipos.xml

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/definicoes/TiposFragment.kt
@@ -0,0 +1,23 @@
+package com.artemissoftware.vvmcontratos.ui.definicoes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.vvmcontratos.R
+
+
+
+
+class TiposFragment : Fragment(R.layout.fragment_tipos) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
+

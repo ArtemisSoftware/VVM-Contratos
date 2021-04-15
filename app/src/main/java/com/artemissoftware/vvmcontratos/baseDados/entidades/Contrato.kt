56c8188d1a41ebf8968ac778329f5183d4ef87de
gustavo <gustavo.isaac@gmail.com>
2021-04-15 18:07:31 +0100

    Entidade - Contrato

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/entidades/Contrato.kt
@@ -0,0 +1,33 @@
+package com.artemissoftware.vvmcontratos.baseDados.entidades
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+
+@Entity(tableName = "contratos")
+data class Contrato(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val uuid: String = UUID.randomUUID().toString().toUpperCase(),
+
+    val idUtilizador: String,
+
+    val nif: String,
+
+    val idTipo: String,
+    val idEmpresa: String,
+    val idMarca: String,
+    val numero: String,
+
+    val sincronizado: Boolean = false
+
+    //val created: Long = System.currentTimeMillis()
+
+) {
+
+//    val createdDateFormatted: String
+//        get() = DateFormat.getDateTimeInstance().format(created)
+}

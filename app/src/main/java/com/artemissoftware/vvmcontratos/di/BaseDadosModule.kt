2f0c6fd72a8f774c5d23d309037d423693a5b86a
gustavo <gustavo.isaac@gmail.com>
2021-04-16 16:22:01 +0100

    BaseDadosModule

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/di/BaseDadosModule.kt
@@ -0,0 +1,25 @@
+package com.artemissoftware.vvmcontratos.di
+
+import android.app.Application
+import androidx.room.Room
+import com.artemissoftware.vvmcontratos.baseDados.BaseDados
+import com.artemissoftware.vvmcontratos.utils.constantes.NOME_BASE_DADOS
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object BaseDadosModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application/*, callback: BaseDados.Callback*/): BaseDados {
+        return Room.databaseBuilder(app, BaseDados::class.java, NOME_BASE_DADOS)
+            .fallbackToDestructiveMigration()
+            //.addCallback(callback)
+            .build()
+    }
+}

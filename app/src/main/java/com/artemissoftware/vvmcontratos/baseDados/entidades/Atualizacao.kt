4fa6cd1d903daacbd34085d81852bd7190ffb7c4
gustavo <gustavo.isaac@gmail.com>
2021-04-16 09:25:41 +0100

    Entidade - Tipo

--- app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/entidades/Atualizacao.kt
+++ app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/entidades/Atualizacao.kt
@@ -3,7 +3,6 @@ package com.artemissoftware.vvmcontratos.baseDados.entidades
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/entidades/Tipo.kt
@@ -0,0 +1,40 @@
+package com.artemissoftware.vvmcontratos.baseDados.entidades
+
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+
+@Entity(
+    tableName = "tipos",
+    indices = [Index(value = ["tipo"], unique = false)],
+    primaryKeys = ["id", "codigo", "idPai"],
+    foreignKeys = [ForeignKey(
+        entity = Atualizacao::class,
+        parentColumns = ["descricao"],
+        childColumns = ["tipo"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class Tipo(
+
+    @NonNull
+    val id: Int,
+
+    @NonNull
+    val tipo: String,
+
+    @NonNull
+    val descricao: String,
+
+    @NonNull
+    val codigo: String,
+
+    @NonNull
+    val idPai: String,
+
+    @NonNull
+    val ativo: Int
+
+) {
+}

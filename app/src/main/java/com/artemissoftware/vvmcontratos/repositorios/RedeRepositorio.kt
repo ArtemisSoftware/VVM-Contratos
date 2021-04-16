b085f3a8cdb45d7ceefeb8f0bfb2c9088f755d3b
gustavo <gustavo.isaac@gmail.com>
2021-04-16 13:17:13 +0100

    RedeRepositorio

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/repositorios/RedeRepositorio.kt
@@ -0,0 +1,10 @@
+package com.artemissoftware.vvmcontratos.repositorios
+
+import com.artemissoftware.vvmcontratos.api.modelos.ListagemDto
+import com.artemissoftware.vvmcontratos.api.modelos.pedido.TipoDto
+import com.artemissoftware.vvmcontratos.utils.Recurso
+
+interface RedeRepositorio {
+
+    suspend fun obterTipo(metodo: String): Recurso<ListagemDto<TipoDto>>
+}

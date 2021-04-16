c99656ae01accb135e8a8b890ba4a437d0213393
gustavo <gustavo.isaac@gmail.com>
2021-04-16 12:41:12 +0100

    ConstantesTeste

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/utils/ConstantesTeste.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.vvmcontratos.utils
+
+
+val NIFS_TESTE = arrayOf(
+    "510210040" /*sem continuidade*/,
+    "102788219" /*com continuidade*/,
+    "505690446" /*renovacao*/,  /*"503171670"*/
+    "502911832" /*cobranca*/
+)
+
+
+val UTILIZADOR_ADMINISTRADOR = "500005"
+val PALAVRA_CHAVE_ADMINISTRADOR = "7765"

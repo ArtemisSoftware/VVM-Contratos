3e5311ac02f6022f8085b109ae3d4575f84bfdd6
gustavo <gustavo.isaac@gmail.com>
2021-04-16 13:02:06 +0100

    Dependencia okhttp3

--- app/src/main/java/com/artemissoftware/vvmcontratos/di/AppModule.kt
+++ app/src/main/java/com/artemissoftware/vvmcontratos/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.artemissoftware.vvmcontratos.di
 
 import com.artemissoftware.vvmcontratos.utils.DispatcherProvider
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
+@Module
+@InstallIn(ApplicationComponent::class)
 object AppModule {
 
     @Singleton

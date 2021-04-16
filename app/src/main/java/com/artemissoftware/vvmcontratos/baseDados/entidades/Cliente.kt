448fef0dd90e38f641420dd0f8b2ca68760b0a07
gustavo <gustavo.isaac@gmail.com>
2021-04-16 11:04:46 +0100

    Entidade - Cliente

---
+++ app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/entidades/Cliente.kt
@@ -0,0 +1,45 @@
+package com.artemissoftware.vvmcontratos.baseDados.entidades
+
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "clientes",
+    foreignKeys = [ForeignKey(
+        entity = Contrato::class,
+        parentColumns = ["id"],
+        childColumns =  ["idContrato"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class Cliente (
+
+    @PrimaryKey
+    val idContrato: Int,
+
+    val idMorada: Int,
+    val nome: String,
+
+    val email: String,
+    val emailAutorizado: Boolean = false,
+
+
+    val idGrupoAtividade: String,
+    val idAtividade1: String,
+    val idAtividade2: String,
+
+
+    val extintores: Boolean = false,
+    val responsavel: String,
+    val nif: String,
+
+    val novo: Boolean = false,
+
+    val nib: String,
+    val niss: String,
+    val segmento: String,
+    val clean: String
+)
+{}

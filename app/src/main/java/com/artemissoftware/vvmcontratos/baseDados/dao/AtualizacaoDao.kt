8a12d489dec0472fdb69c122d29517c2b9a75b15
gustavo <gustavo.isaac@gmail.com>
2021-04-17 18:19:57 +0100

    AtualizacaoDao

--- app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/dao/AtualizacaoDao.kt
+++ app/src/main/java/com/artemissoftware/vvmcontratos/baseDados/dao/AtualizacaoDao.kt
@@ -3,19 +3,13 @@ package com.artemissoftware.vvmcontratos.baseDados.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import com.artemissoftware.vvmcontratos.baseDados.entidades.Atualizacao
 import com.artemissoftware.vvmcontratos.baseDados.entidades.Tipo
-import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface TipoDao {
-
-
-    @Query("SELECT * FROM tipos WHERE tipo =:tipo  AND ativo = 1 ORDER BY id ASC ")
-    fun obterTipo(tipo: String): Flow<List<Tipo>>
+interface AtualizacaoDao {
 
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(tipos: List<Tipo>)
-
+    suspend fun insert(atualizacao: Atualizacao)
 }
--- app/src/main/java/com/artemissoftware/vvmcontratos/ui/definicoes/TiposFragment.kt
+++ app/src/main/java/com/artemissoftware/vvmcontratos/ui/definicoes/TiposFragment.kt
@@ -1,14 +1,20 @@
 package com.artemissoftware.vvmcontratos.ui.definicoes
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.artemissoftware.vvmcontratos.R
 import com.artemissoftware.vvmcontratos.api.MetodoTipos
+import com.artemissoftware.vvmcontratos.utils.BaseViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tipos.*
+import kotlinx.coroutines.flow.collect
 
 
 @AndroidEntryPoint
@@ -22,6 +28,35 @@ class TiposFragment : Fragment(R.layout.fragment_tipos) {
 
 
         viewModel.obterTipo(MetodoTipos.EMPRESAS_VIVAMAIS)
+
+        subscreverObservadores()
+
+    }
+
+    private fun subscreverObservadores() {
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.evento.collect { evento ->
+
+                when(evento) {
+
+                    is BaseViewModel.Evento.Sucesso -> {
+                        loading.isVisible = false
+
+                    }
+
+                    is BaseViewModel.Evento.Erro -> {
+                        loading.isVisible = false
+
+                    }
+
+                    is BaseViewModel.Evento.Loading -> {
+                        loading.isVisible = true
+                    }
+                    else -> Unit
+                }
+            }
+        }
     }
 
 }
